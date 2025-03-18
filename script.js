7e1bc4d3875be37d99a3dbf6d653ae00eb46fc52
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-03-18 22:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here!
+const infiList = document.getElementById("infi-list");
+function addListItems(num){
+	for(let i=0;i<num;i++){
+		const li = document.createElement("li");
+		 li.textContent = `List Item ${infiList.children.length + 1}`;
+    infiList.appendChild(li);
+  }
+}
+addListItems(10);
 
+// Detect when the user scrolls to the end of the list
+infiList.addEventListener("scroll", () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+    // Add 2 more list items when the user reaches the end
+    addListItems(2);
+  }
+});
